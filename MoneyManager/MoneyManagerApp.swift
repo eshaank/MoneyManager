b0b57bbc521cd24b6704ec772f9bde0bcfad51aa
eshaank <eshaank@gmail.com>
2024-08-17 16:59:40 -0700

    Initial Commit

---
+++ MoneyManager/MoneyManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyManagerApp.swift
+//  MoneyManager
+//
+//  Created by Eshaan Kansagara on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MoneyManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
